323bc09fa53ec6a200f7ca204c7f0a9ad01c0efc
Dmitry Koryanov <dmitry.koryanov@yandex.ru>
2020-10-01 14:13:02 +0300

    Sprint 2. The code and the project description have been completed.

--- Phase1/src/virtualkey/UserInterface.java
+++ Phase1/src/virtualkey/UserInterface.java
@@ -2,21 +2,46 @@ package virtualkey;
 
 import java.io.Console;
 
+//the main class to display the interface for the user
 public class UserInterface {
 	
+	//bbolean var to control the the correctness of the option chosen
 	private static boolean IsUserAnswerCorrect = false;
 	
+	//user's chosen option
 	static char user_answer;
 	
+	//the console being diaplayed - either 1 or 2
 	static short console_to_display = 1;
-	
-	// get the console
+
+	//enumeration was added just to make the code nicer & more understandable, in fact is not required
+	public enum UserChoice {
+		//three possible actions with corresponding text for the text messages
+		ADD("created"), DELETE("deleted"), SEARCH("searched for");
+		
+		public String action;
+
+		//private constructor to align the text message ("action") with the option selected
+		private UserChoice(String string) {
+			this.action = string;
+		}
+		
+		//return the text description of the action selected for the text messages
+		public String getAction() {
+			return this.action;
+		}
+		   
+	}
+
+	// get the system console
 	static Console console = System.console();	
 	
+	//just a setter
 	public static void setIsUserAnswerCorrect(boolean isUserAnswerCorrect) {
 		IsUserAnswerCorrect = isUserAnswerCorrect;
 	}
 
+	//Displays either first or second menu
 	public static void DisplayMenu() {
 		
 		//just for the nicer design
@@ -25,7 +50,7 @@ public class UserInterface {
 		
 		if(console_to_display == 1) {
 			
-		   //Displaying the main menu
+		   //Displaying the main (first) menu
 		   console.writer().println("Main Menu - please select one of the options:");
 		   console.writer().println("[1] - Display the list of files in ascending order.");
 		   console.writer().println("[2] - Work with files directory.");
@@ -50,15 +75,18 @@ public class UserInterface {
 	
 	}
 	
+	//reads and verifies user's answer
 	public static boolean GetTheAnswer() {
 		
 		//Getting the answer from the user (the first symbol)
 		String users_choice = console.readLine();
 		
+		//Just pressing the "enter" us nit allowed
 		if(users_choice.length() == 0) {
 			WrongOption("Error: No characters typed. Please type at least one character!");
 			return false;
-		}			
+		}
+		//all options are of length "1", hence answers with greater length are not being accepted
 		else if(users_choice.length() > 1) {
 			WrongOption("Error: Wrong number of characters typed. Please type only one character!");
 			return false;
@@ -69,11 +97,13 @@ public class UserInterface {
 		
 	}
 	
+	//function to terminate the application in case a corresponding choice has been made
 	public static void ExitFromTheApplication() {
 		System.out.println("Exiting from the application.. Many thanks and bye!");
 		System.exit(0);
 	}
 	
+	//error message about wrong option chosen
 	public static void WrongOption(String ErrorMessage) {
 		
 		System.err.println();		
@@ -83,6 +113,7 @@ public class UserInterface {
 		System.err.println("----------Attention!------------------");		
 	}
 
+	//main method
 	public static void main(String[] args) {
 		
 		//Printing an error message in case console is not available for any reason
@@ -98,40 +129,47 @@ public class UserInterface {
 		console.writer().println("Java Developer: Dmitry Koryanov");
 		console.writer().println("////////////////////////////////////////////////");
 		console.writer().println();
-				
+		
+		//main do-while loop
 		do {
 			
+			//display the menu
 			DisplayMenu();
+			//get user's answer
 			IsUserAnswerCorrect = GetTheAnswer();
+			//in case no correct option is selected, continue
 			if(!IsUserAnswerCorrect) continue;
 			
-			if (console_to_display == 1) {
+			//switch statements with corresponding actions for both menus
+			if (console_to_display == 1) { //first (main) menu (console)
 			   switch(user_answer) {
-			      case '1': 
+			      case '1': //display files in ascending order
 				         WorkWithUserFiles.DisplayFiles();
 			             break;
-			      case '2': 
+			      case '2': //enter console 2
 				         console_to_display = 2;
 			             break;
-			      case '3': 
+			      case '3': //terminate the application
 				         ExitFromTheApplication();
 			             break;
+			      //wrong option selected       
 			      default: WrongOption("Error: Not existing option selected. Please select either [1] or [2] or [3]!");
 			   }
-			} else if (console_to_display == 2) {
+			} else if (console_to_display == 2) { //second menu (console)
 				   switch(user_answer) {
-				      case 'a': 
-					         WorkWithUserFiles.AddAFile();
+				      case 'a': //add a file
+					         WorkWithUserFiles.AddDeleteSearchAFile(UserChoice.ADD);
 				             break;
-				      case 'd': 
-				    	     WorkWithUserFiles.DeleteAFile();
+				      case 'd': //delete a file
+				    	     WorkWithUserFiles.AddDeleteSearchAFile(UserChoice.DELETE);
 				             break;
-				      case 's': 
-				    	     WorkWithUserFiles.SearchForAFile();
+				      case 's': //search for a file
+				    	     WorkWithUserFiles.AddDeleteSearchAFile(UserChoice.SEARCH);
 				             break;
-				      case 'e':
+				      case 'e': //return to console 1
 				    	     console_to_display = 1;
 				    	     break;
+				      //wrong option selected     
 				      default: WrongOption("Error: Not existing option selected. Please select either [a] or [b] or [s] or [e]!");
 				   }				
 			}
--- Phase1/src/virtualkey/WorkWithUserFiles.java
+++ Phase1/src/virtualkey/WorkWithUserFiles.java
@@ -1,21 +1,207 @@
 package virtualkey;
 
+//import of necessary packages
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//class to perform necessary actions on files in root folder
 public class WorkWithUserFiles {
 	
-	public static void DisplayFiles() {
-		System.out.println("displaying the files");
-	}
+	//our file storage folder
+	private static File root_dir = new File("../root");
+	
+	//entered by the user file name
+	private static String user_file_name;
 
-	public static void AddAFile() {
-		System.out.println("adding a file");
+	//function to print a failure message
+	public static void printError(String message) {
+		System.err.println();		
+		System.err.println("----------Attention!------------------");
+		System.err.println(message);
+		System.err.println("----------Attention!------------------");			
+	}
+	
+	//function to print a success message
+	public static void printSuccess(String message) {
+		System.err.println();		
+		System.err.println("----------Success!------------------");
+		System.err.println(message);
+		System.err.println("You have returned to the menu to work with files!");
+		System.err.println("----------Success!------------------");			
 	}	
 	
-	public static void DeleteAFile() {
-		System.out.println("deleting a file");
+	//Safe way to get absolute path of the file. In case of an exception the file name itself is being returned.
+	public static String getAbsolutePath (File file) {
+		
+		String AbsolutePath = "";
+		
+		try { 
+			AbsolutePath = file.toPath().toAbsolutePath().normalize().toString();
+		} catch (InvalidPathException | IOError | SecurityException ex) {
+			AbsolutePath = file.getName();
+		}
+			
+		return AbsolutePath;
+	}
+	
+	//Check for the existence of the file
+	public static boolean CheckFileExistence(File file) {
+		
+		boolean file_exists = false;
+		
+		try {
+		   file_exists = file.exists();	
+		} catch (SecurityException ex){
+			printError("Error: an unexpected error occured while trying to check the existence of the file: "+file.getName()+" Error message: "+ex.getMessage());
+		}
+		
+		return file_exists;
+	}
+	
+	//function to check if the file storage folder ("root") exists
+	public static boolean RootDirExists() {
+		return CheckFileExistence(root_dir);
+	}
+	
+	//Function to add, delete or search for a file
+	public static void AddDeleteSearchAFile(UserInterface.UserChoice user_choice) {
+		
+		boolean b = false; //boolean flag for the loop end
+		
+		//main loop of the action
+		do {
+			
+			//always check if root folder exists before each action
+			if(!RootDirExists()) {
+				printError("'root' directory can't be found. Please create the directory if it doesn't exist.");
+				return;
+			}
+			
+			//get the text description of the action for text messages for different actions - add, delete or search
+			String action = user_choice.getAction();
+			
+			//display the message to the user
+			UserInterface.console.writer().println();
+			UserInterface.console.writer().println("-----------------------------------------------");
+			UserInterface.console.writer().println("Please type a file name to be "+user_choice.getAction()+" or type 'exit' to return to the menu to work with files:");
+			UserInterface.console.writer().println("-----------------------------------------------");		
+			UserInterface.console.writer().println();
+			
+			//get the file name entered
+			user_file_name = UserInterface.console.readLine();
+			
+			//control the answer from user
+			if(user_file_name.length() == 0) {
+				printError("No characters typed. Please type at least one character!");
+				continue;
+			} else if (user_file_name.equalsIgnoreCase("exit")) //if "exit" was typed, returning to the menu 
+				break;
+			
+			//creating a file object
+			File new_file = new File(root_dir, "/"+user_file_name);
+			
+			boolean file_exists = false;
+			
+			//check for the existence of the file
+			file_exists = CheckFileExistence(new_file);
+			
+			//perform necessary checks for ADD & DELETE actions
+			//display an error message if file exists and we have add action
+			if(file_exists && user_choice.equals(UserInterface.UserChoice.ADD)) {
+				printError("File \""+user_file_name+"\" already exists. Please chose a different name for the file to be created.");
+				continue;
+			//display an error message if file doesn't exist and we have delete action	
+			} else if (!file_exists && user_choice.equals(UserInterface.UserChoice.DELETE)) {
+				printError("File \""+user_file_name+"\" does not exist. Please chose a different name for the file to be deleted.");
+				continue;				
+			}
+
+			//In case search menu option is chosen we search for a file 
+			if(user_choice.equals(UserInterface.UserChoice.SEARCH)) {
+				
+				if(file_exists) { //success, file is found
+					printSuccess("The file \""+user_file_name+"\" with the possible absolute path \""+getAbsolutePath(new_file)+"\" has been found!");					
+				} else { //file is not found
+					printError("The file \""+user_file_name+"\" with the absolute path \""+getAbsolutePath(new_file)+"\" has not been found!");										
+				}
+
+				break;
+			}
+			
+			try {
+				if(user_choice.equals(UserInterface.UserChoice.ADD))
+			       b = new_file.createNewFile(); //adding a file to the directory
+				else if(user_choice.equals(UserInterface.UserChoice.DELETE))
+				   b = new_file.delete(); //deleting a file from the directory
+			} catch (IOException | SecurityException ex) {
+				printError("Error: an unexpected error occured while working with a file: "+user_file_name+" Error message: "+ex.getMessage());
+				break;
+			}
+			
+			if(b) //in case of a success (it means end of the loop) print a corresponding mesaage
+				printSuccess("The file \""+user_file_name+"\" has been successfully "+user_choice.getAction());
+			
+		} while (!b);
 	}	
 	
-	public static void SearchForAFile() {
-		System.out.println("searching for a file");
-	}		
+	//function to display list of the files in the "root" folder in the ascending order
+	public static void DisplayFiles() {
+
+		//always check if root folder exists before each action
+		if(!RootDirExists()) {
+			printError("'root' directory can't be found. Please create the directory if it doesn't exist.");
+			return;
+		}
+
+		//display the text message
+		UserInterface.console.writer().println();
+		UserInterface.console.writer().println("-----------------------------------------------");
+		UserInterface.console.writer().println("List of the files found in the directory:");
+		UserInterface.console.writer().println("-----------------------------------------------");		
+		UserInterface.console.writer().println();
+		
+		int i = 0;
+		
+		File[] files;
+		
+		try {
+		   files = root_dir.listFiles(); //getting the list of files in the directory
+		} catch (SecurityException ex) {
+			printError("Error: an unexpected error occured while trying to get the list of files in the directory."+ex.getMessage());
+			return;
+		}
+		
+		List<File> filesList = new ArrayList<File>();
+		
+		if (files.length > 0) { //if there are files in the directory
+		   Collections.addAll(filesList, files); //copy the list of files to a collection
+		   Collections.sort(filesList); //sort the list in the ascending order
+		}   
+		
+		for(File file:filesList) { //printing the results - found files
+			System.out.println("File \""+file.getName()+"\" has been found. The possible absolute path is \""+getAbsolutePath(file)+"\"");
+			i++;
+		}
+		
+		//end of the message to user
+		UserInterface.console.writer().println();
+		UserInterface.console.writer().println("-----------------------------------------------");
+		
+		//The number of files found is being printed as well
+		if (i>0)
+			UserInterface.console.writer().println("Totally files has been found in the directory: "+i);
+		else
+			UserInterface.console.writer().println("No files has been found in the directory.");
+		
+		UserInterface.console.writer().println("You are being returned to the main menu.");
+		UserInterface.console.writer().println("-----------------------------------------------");		
+		UserInterface.console.writer().println();		
+		
+	}
 	
 }
